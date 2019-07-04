10cf815a615187ab03ef057b5cad5a17ce02238a
Josh Powell <josh.powell@softwire.com>
2019-07-04 14:47:09 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+class App{
+    init(){
+
+        const PORT = process.env.HTTP_PORT || 4001;
+        const app = express();
+
+        app.get('/', (req, res) => {
+        res.send('just gonna send it');
+        });
+
+        app.get('/flower', (req, res) => {
+        res.json({
+        name: 'Dandelion',
+        colour: 'Blue-ish'
+        });
+        });
+
+        app.listen(PORT, () => {
+        console.log(`Server listening at port ${PORT}.`);
+    });
+}
+}
+
+let program = new App()
+program.init();
