f9fb1093a37c6fa36343c6def124005d54e6195e
Charlie Quin <charliequin@outlook.com>
2019-07-08 17:13:03 +0100

    Added code embeds

--- client/src/components/Framework/Framework.jsx
+++ client/src/components/Framework/Framework.jsx
@@ -15,32 +15,34 @@ export default class Framework extends React.Component {
 					<h5>Reference material used for designing the styling for CAC.</h5>
 				</div>
 
-				<br/>
+				<div className="section">
+					<div className="white-card">
+						<div className="white-card-body">
+							My abusive boyfriend who really isn't so bad once you get to know him.
+						</div>
+					</div>
+					<iframe src="https://codepen.io/charliequin/embed/XLyKPR"></iframe>
+				</div>
 
-				<div className="white-card">
-					<div className="white-card-body">
-						My abusive boyfriend who really isn't so bad once you get to know him.
+				<div className="section">
+					<div className="black-card">
+						<div className="black-card-body">
+							There is no God. It's just _____________ and then you die. 
+						</div>
+					</div>
+					<iframe src="https://codepen.io/charliequin/embed/RzqREE"></iframe>
+				</div>
+
+				<div className="section">
+					<div className="scoreboard">
+						<div className="sb-round">
+						</div>
+						<div className="sb-players">
+						</div>
 					</div>
 				</div>
 
-				<br/>
 
-				<div className="black-card">
-					<div className="black-card-body">
-						There is no God. It's just _____________ and then you die. 
-					</div>
-				</div>
-
-				<br/>
-
-				<div className="scoreboard">
-					<div className="sb-round">
-					</div>
-					<div className="sb-players">
-					</div>
-				</div>
-
-				
 			</div>
 			</>
     )
