b0d006f44c3f4d115b90c6ac969bdf460d332822
Millie <millie_clare@outlook.com>
2019-07-09 15:30:03 +0100

    tests for czar added

--- api/services/roundService.js
+++ api/services/roundService.js
@@ -1,56 +1,57 @@
 
 export default class RoundService {
-    constructor(){
+    constructor() {
         this.players = [{
-            username: shaquille_oatmeal,
+            username: 'shaquille_oatmeal',
             score: 8,
             ID: 1
-        },{
-            username: dildo_swaggins,
+        }, {
+            username: 'dildo_swaggins',
             score: 6,
             ID: 2
         },
         {
-            username: in_jail_out_soon,
+            username: 'in_jail_out_soon',
             score: 7,
             ID: 3
         },
         {
-            username: kiss-my-axe,
+            username: 'kiss - my - axe',
             score: 3,
             ID: 4
         },
         {
-            username: hugs_for_drugs,
+            username: 'hugs_for_drugs',
             score: 9,
             ID: 5
         }
-            ];
+        ];
+
     }
 
-    newRound(){
+    newRound() {
         getCzar(newGame)//TODO: newgame is a boolean that is passed in
 
     }
 
-    endRound(){
+    endRound() {
 
     }
 
-    getCzar(newGame){
-        if(newGame){
+    getCzar(newGame) {
+        console.log(this.players.length);
+        if (newGame) {
             this.currentCzarNum = Math.floor(Math.random() * Math.floor(this.players.length));
-            this.currentCzar = this.players[this.currentCzarNum];
+            this.currentCzar = this.players[this.currentCzarNum - 1];
             return this.currentCzar;
         } else {
-            if(this.currentCzarNum === this.players.length){
+            if (this.currentCzarNum === this.players.length) {
                 this.currentCzarNum = 0
-                this.currentCzar = this.currentCzarNum;
-                return this.currentCzar;
             } else {
-                this,currentCzar = this.player[this.currentCzarNum++];
-                return this.currentCzar;
+                this.currentCzarNum++
             }
+            this.currentCzar = this.players[this.currentCzarNum]
+            return this.currentCzar;
         }
     }
 }
--- api/tests/testsService.test.js
+++ api/tests/testsService.test.js
@@ -1,4 +1,5 @@
 import TestService from '../services/testService';
+import roundService from '../services/roundService';
 
 test('adds two positive integers', () => {
     // ARRANGE
@@ -9,4 +10,53 @@ test('adds two positive integers', () => {
     
     // ASSERT
     expect(result).toBe(4)
+})
+
+test('if new game, get a random player to be czar', () => {
+    const mockMath = Object.create(global.Math);
+    mockMath.random = () => 1;
+    global.Math = mockMath;
+    
+    const service = new roundService();
+
+    const result = service.getCzar(true);
+
+    expect(result).toStrictEqual(
+        {
+            username: 'hugs_for_drugs',
+            score: 9,
+            ID: 5
+        });
+})
+
+test('if not new game, get next player in array to be czar', () => {
+    
+    const service = new roundService();
+    service.currentCzarNum = 3; 
+
+    const result = service.getCzar(false);
+
+
+    expect(result).toStrictEqual(
+        {
+            username: 'hugs_for_drugs',
+            score: 9,
+            ID: 5
+        });
+})
+
+test('if end of player list, reset to 0', () => {
+    
+    const service = new roundService();
+    service.currentCzarNum = 5;
+
+    const result = service.getCzar(false);
+
+
+    expect(result).toStrictEqual(
+        {
+            username: 'shaquille_oatmeal',
+            score: 8,
+            ID: 1
+        });
 })
