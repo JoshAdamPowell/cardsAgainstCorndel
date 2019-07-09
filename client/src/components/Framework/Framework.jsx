d51cf6b1570ae01e4bc3e12070f916fdbfd0b96b
Charlie Quin <charliequin@outlook.com>
2019-07-09 13:00:36 +0100

    Added card styling tests / Added White + Black card JSON

--- client/src/components/Framework/Framework.jsx
+++ client/src/components/Framework/Framework.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import './css/Cards.css'
+import './css/Cards.css';
+import blackCards from './black-cards.js'
+import whiteCards from './white-cards.js'
 
 export default class Framework extends React.Component {
   constructor(props){
-      super(props)
+			super(props)
+			this.state = {
+				blackCard: null,
+				whiteCard: null
+			}
   }
+
+  componentDidMount(){
+		document.body.classList.toggle('dark-body', this.props.isDark)
+		this.changeBlackBody();
+		this.changeWhiteBody();
+	}
+	
+	changeBlackBody(){
+		let ranBlackCard = blackCards[Math.floor(Math.random() * blackCards.length)];
+		this.setState({
+			blackCard: ranBlackCard.text
+		})
+	}
+
+	changeWhiteBody(){
+		let ranWhiteCard = whiteCards[Math.floor(Math.random() * whiteCards.length)];
+		this.setState({
+			whiteCard: ranWhiteCard
+		})
+	}
 
   render(){
     return (
@@ -17,20 +43,22 @@ export default class Framework extends React.Component {
 
 				<div className="section">
 					<div className="white-card">
-						<div className="white-card-body">
-							My abusive boyfriend who really isn't so bad once you get to know him.
+						<div className="white-card-body" dangerouslySetInnerHTML={{__html: this.state.whiteCard}}>
 						</div>
 					</div>
-					<iframe src="https://codepen.io/charliequin/embed/XLyKPR"></iframe>
+					<div className="section-side">
+						<iframe className="embed" src="https://codepen.io/charliequin/embed/XLyKPR"></iframe>
+						<button className="btn-changer" onClick={() => this.changeWhiteBody()} type="submit">Test Body</button>
+					</div>
 				</div>
 
 				<div className="section">
 					<div className="black-card">
-						<div className="black-card-body">
-							There is no God. It's just _____________ and then you die. 
+						<div className="black-card-body" dangerouslySetInnerHTML={{__html: this.state.blackCard}}>
 						</div>
 					</div>
-					<iframe src="https://codepen.io/charliequin/embed/RzqREE"></iframe>
+					<button className="btn-changer" onClick={() => this.changeBlackBody()} type="submit">Test Body</button>
+					{/* <iframe src="https://codepen.io/charliequin/embed/RzqREE"></iframe> */}
 				</div>
 
 				<div className="section">
@@ -41,8 +69,6 @@ export default class Framework extends React.Component {
 						</div>
 					</div>
 				</div>
-
-
 			</div>
 			</>
     )
