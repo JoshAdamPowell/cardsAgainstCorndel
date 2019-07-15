88b9f71decac1d4bbe6e94079b67b87542683834
Charlie Quin <charliequin@outlook.com>
2019-07-15 17:15:41 +0100

    Initial commit

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export default class Game extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    render() {
-        return (
-                <div className="game-heading">
-                    <h1>Lets play</h1>
-                </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+          <h2>Lets play!</h2>
+      </div>
+    )
+  }
 }
